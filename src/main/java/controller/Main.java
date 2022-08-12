208981a007122d34651744117066f3309d35fd04
hieu nguyen <hieunguyenmanh13.cdc@gmail.com>
2022-08-12 13:04:10 +0700

    update product

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,5 +1,8 @@
 package controller;
 
+import model.Product;
+import service.ProductService;
+import service.impl.ProductServiceImplement;
 import util.DBUtil;
 
 import java.math.BigDecimal;
@@ -10,22 +13,5 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
-        DBUtil dbconect= DBUtil.getInstance();
-        Connection connection= dbconect.getConnection();
-        String sql= "INSERT INTO Products(Name, Description, Price, Discount_Price, Stock, Sold, Create_date, Status) VALUES(?,?,?,?,?,?,?,?)";
-        PreparedStatement preparedStatement= connection.prepareStatement(sql);
-        preparedStatement.setString(1,"Test1");
-        preparedStatement.setString(2,"Test1");
-        preparedStatement.setBigDecimal(3, BigDecimal.valueOf(5000));
-        preparedStatement.setBigDecimal(4,BigDecimal.valueOf(5000));
-        preparedStatement.setInt(5,10);
-        preparedStatement.setInt(6,3);
-        preparedStatement.setString(7,"2021/05/05");
-        preparedStatement.setInt(8,0);
 
-
-        preparedStatement.executeUpdate();
-
-    }
 }
